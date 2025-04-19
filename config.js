81bef91bdb8287f5c2648f380ff2f4e9655e7492
NikooGram <nikoogram807@gmail.com>
2025-04-19 04:28:25 +0200

    Update

--- config.js
+++ config.js
@@ -5,12 +5,14 @@ module.exports = {
     prefix: process.env.PREFIX || '!', // Prefijo para los comandos
     channels: {
         support: process.env.SUPPORT_CHANNEL_ID, // Canal de soporte para tickets
-        welcome: process.env.WELCOME_CHANNEL_ID, // Canal de bienvenidas
+        welcome: process.env.WELCOME_CHANNEL_ID || 'bienvenidas', // Canal de bienvenidas
         transcriptLog: process.env.TRANSCRIPT_LOG_CHANNEL_ID || 'registro', // Canal para transcripciones
-        sorteoLog: process.env.SORTEO_LOG_CHANNEL_ID,
+        sorteoLog: process.env.SORTEO_LOG_CHANNEL_ID, // Canal para registrar sorteos
+        sugerenciaLog: process.env.SUGERENCIA_LOG_CHANNEL_ID, // canal para registrar sugerencias
     },
     roles: {
         staff: process.env.STAFF_ROLE_ID, // Rol de Staff
+        asistente: process.env.ASISTENTE_ROLE_ID, // Rol de Asistente
     },
     categories: {
         ticket: process.env.TICKET_CATEGORY_ID, // Categoría para tickets
